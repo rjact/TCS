8012515c2adefe7c3863c2f610ca704ec4bf78e8
Ray Acunto <ray@upgradewebdev.com>
2013-10-30 22:55:37 -0400

    highlight work tab when you get to that page (but it's a bit buggy)

--- js/index.js
+++ js/index.js
@@ -35,6 +35,8 @@ $(document).ready(function() {
 		
 		controller.pin(sections[WORK], 8000, {
 				anim: (new TimelineLite())
+					.call(function() {$('.navitem').removeClass('active');})
+					.call(function() {$('#nav_work').addClass('active');})
 					.append(TweenLite.from(sections[WORK].find('.sec_header'), 1, {marginTop:'-420px'}))
 					.append(TweenMax.staggerFrom(sections[WORK].find('.work_example'), 1, {y:'-100',opacity:0, ease:Expo.easeOut}, .5))
 		, offset: -60});
