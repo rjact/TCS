74882bd0b37720ec7eb71621c7722da0b02c4d67
Ray Acunto <ray@upgradewebdev.com>
2013-11-01 16:36:51 -0400

    undid last commit, because that wasn't necessary

--- js/index.js
+++ js/index.js
@@ -6,7 +6,9 @@ $(document).ready(function() {
 		TweenLite.to($('#sec_home h1'), 1.75, {css: {color: '#ffffff'}}),
 		TweenLite.delayedCall(1.5, function() {
 			$('#intro-image').css('backgroundPosition', 'bottom center');	
-		})]);
+		})
+		
+		]);
 	
 	function initScroller() {
 		var controller = $.superscrollorama();
@@ -33,13 +35,52 @@ $(document).ready(function() {
 		//stick nav bar to top
 		controller.addTween($(window).height()+60, TweenMax.to($('nav'), .001, {css:{position:'fixed',top:'0%'}}),1);
 		
-		controller.pin(sections[WORK], 8000, {
+		
+		//work section
+		controller.pin(sections[WORK], 2000, {
 				anim: (new TimelineLite())
 					.call(function() {$('.navitem').removeClass('active');})
 					.call(function() {$('#nav_work').addClass('active');})
-					.append(TweenLite.from(sections[WORK].find('.sec_header'), 1, {marginTop:'-420px'}))
+					.append(TweenLite.from(sections[WORK].find('.sec_header'), 1, {marginTop:'-420px'}, -500))
 					.append(TweenMax.staggerFrom(sections[WORK].find('.work_example'), 1, {y:'-100',opacity:0, ease:Expo.easeOut}, .5))
 		, offset: -60});
+		
+		//services page
+		controller.pin(sections[SERVICES], 2000, {
+				anim: (new TimelineLite())
+					.call(function() {$('.navitem').removeClass('active');})
+					.call(function() {$('#nav_services').addClass('active');})
+					.append(TweenMax.staggerFrom(sections[SERVICES].find('.services_example'), 1, {y:'-100', scaleY:-1, alpha:0, ease:Expo.easeOut}, .5))
+		, offset: -60});
+		
+		//clients page
+		controller.pin(sections[CLIENTS], 2000, {
+				anim: (new TimelineLite())
+					.call(function() {$('.navitem').removeClass('active');})
+					.call(function() {$('#nav_clients').addClass('active');})
+					.append(TweenMax.staggerFrom(sections[CLIENTS].find('.client_example'), 1, {x:'+1000', ease:Expo.easeOut}, .5))
+		, offset: -60});
+		
+		//team page
+		controller.pin(sections[TEAM], 2000, {
+				anim: (new TimelineLite())
+					.call(function() {$('.navitem').removeClass('active');})
+					.call(function() {$('#nav_team').addClass('active');})
+					.append(TweenMax.staggerFrom(sections[TEAM].find('.team_example'), 1, {y:'+900', ease:Expo.easeOut}, .5))
+		, offset: -60});
+		
+		//team page
+		/* well, this is all f'ed up
+		controller.pin(sections[CAREERS], 3000, {
+				anim: (new TimelineLite())
+					.call(function() {$('.navitem').removeClass('active');})
+					.call(function() {$('#nav_careers').addClass('active');})
+					.append(TweenMax.from(sections[CAREERS].find('.sec_content'), 1, {alpha:0, ease:Expo.easeOut}, .5))
+		, offset: -60});
+		*/
+		
+		TweenLite.to(window, 5, {scrollTo:{y:$('#sec_work').position().top}})
+		TweenLite.to(window, 2.5, {scrollTo:{y:$('#sec_work').position().top+1500}})
 	}
 	
 });
