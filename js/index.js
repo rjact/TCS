ea8072e8cccb826381dfd33a4f7f84f6e1b829b2
Ray Acunto <ray@upgradewebdev.com>
2013-11-01 16:01:43 -0400

    tried getting scrolling working

--- js/index.js
+++ js/index.js
@@ -6,7 +6,9 @@ $(document).ready(function() {
 		TweenLite.to($('#sec_home h1'), 1.75, {css: {color: '#ffffff'}}),
 		TweenLite.delayedCall(1.5, function() {
 			$('#intro-image').css('backgroundPosition', 'bottom center');	
-		})]);
+		})
+		
+		]);
 	
 	function initScroller() {
 		var controller = $.superscrollorama();
@@ -39,7 +41,7 @@ $(document).ready(function() {
 				anim: (new TimelineLite())
 					.call(function() {$('.navitem').removeClass('active');})
 					.call(function() {$('#nav_work').addClass('active');})
-					.append(TweenLite.from(sections[WORK].find('.sec_header'), 1, {marginTop:'-420px'}))
+					.append(TweenLite.from(sections[WORK].find('.sec_header'), 1, {marginTop:'-420px'}, -500))
 					.append(TweenMax.staggerFrom(sections[WORK].find('.work_example'), 1, {y:'-100',opacity:0, ease:Expo.easeOut}, .5))
 		, offset: -60});
 		
@@ -76,6 +78,9 @@ $(document).ready(function() {
 					.append(TweenMax.from(sections[CAREERS].find('.sec_content'), 1, {alpha:0, ease:Expo.easeOut}, .5))
 		, offset: -60});
 		*/
+		
+		TweenLite.to(window, 3, {scrollTo:{y:$('#sec_work').position().top}})
+		TweenLite.to(window, 2.5, {scrollTo:{y:$('#sec_work').position().top+7000}})
 	}
 	
 });
