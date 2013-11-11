c7698692f6d060afadbcb54e2e70fa7134719528
Ray Acunto <ray@upgradewebdev.com>
2013-11-10 19:08:47 -0500

    got rid of unneeded code

--- js/index.js
+++ js/index.js
@@ -3,8 +3,6 @@ var PIN_DURATION = 2000
 $(document).ready(function () {
 	$(document).scrollTop(0);
 	tlIntro = new TimelineLite();
-	//tlIntro = new TimelineLite({ onComplete: initScroller });
-	//tlIntro = new TimelineLite({ onComplete: s.animateTo($('#work').position().top) });
 	tlIntro.delay(.01).append([
 		TweenLite.to($('#home'), 1.5, { css: { backgroundColor: '#002B45'} }),
 		TweenLite.to($('#home h1'), 1.75, { css: { color: '#ffffff'} }),
@@ -30,105 +28,6 @@ $(document).ready(function () {
 		offset: 0,
 		duration:1500
 	});
-
-	$('section.sec').each(function () {
-		$(this).css('height', $(this.outerHeight) + PIN_DURATION);
-	});
-
-	function initScroller() {
-		var controller = $.superscrollorama();
-
-		//controller.addTween('#sec_home', TweenLite.to($('#sec_home'), .5, {css:{opacity:.3}}));
-		var HOME = 0,
-			WORK = 1,
-			SERVICES = 2,
-			CLIENTS = 3,
-			TEAM = 4,
-			CAREERS = 5,
-			CONTACT = 6
-		var sections = new Array();
-		sections[HOME] = $('#sec_home');
-		sections[WORK] = $('#sec_work');
-		sections[SERVICES] = $('#sec_services');
-		sections[CLIENTS] = $('#sec_clients');
-		sections[TEAM] = $('#sec_team');
-		sections[CAREERS] = $('#sec_careers');
-		sections[CONTACT] = $('#sec_contact');
-		var nav = $('nav');
-
-
-		//stick nav bar to top
-		controller.addTween($(window).height() + 60, TweenMax.to($('nav'), .001, { css: { position: 'fixed', top: '0%'} }), 1);
-
-
-		//work section
-		controller.pin(sections[WORK], PIN_DURATION, {
-			anim: (new TimelineLite())
-					.call(function () { $('.navitem').removeClass('active'); })
-					.call(function () { $('#nav_work').addClass('active'); })
-					.append(TweenLite.from(sections[WORK].find('.sec_header'), 1, { marginTop: '-420px' }, -500))
-					.append(TweenMax.staggerFrom(sections[WORK].find('.work_example'), 1, { y: '-100', opacity: 0, ease: Expo.easeOut }, .5))
-		, offset: -100
-		});
-
-		//services page
-		controller.pin(sections[SERVICES], PIN_DURATION, {
-			anim: (new TimelineLite())
-					.call(function () { $('.navitem').removeClass('active'); })
-					.call(function () { $('#nav_services').addClass('active'); })
-					.append(TweenMax.staggerFrom(sections[SERVICES].find('.services_example'), 1, { y: '-100', scaleY: -1, alpha: 0, ease: Expo.easeOut }, .5))
-		, offset: -100
-		});
-
-		//clients page
-		controller.pin(sections[CLIENTS], PIN_DURATION, {
-			anim: (new TimelineLite())
-					.call(function () { $('.navitem').removeClass('active'); })
-					.call(function () { $('#nav_clients').addClass('active'); })
-					.append(TweenMax.staggerFrom(sections[CLIENTS].find('.client_example'), 1, { x: '+1000', ease: Expo.easeOut }, .5))
-		, offset: -100
-		});
-
-		//team page
-		controller.pin(sections[TEAM], PIN_DURATION, {
-			anim: (new TimelineLite())
-					.call(function () { $('.navitem').removeClass('active'); })
-					.call(function () { $('#nav_team').addClass('active'); })
-					.append(TweenMax.staggerFrom(sections[TEAM].find('.team_example'), 1, { y: '+900', ease: Expo.easeOut }, .5))
-		, offset: -100
-		});
-
-		//career page
-		controller.pin(sections[CAREERS], PIN_DURATION, {
-			anim: (new TimelineLite())
-					.call(function () { $('.navitem').removeClass('active'); })
-					.call(function () { $('#nav_careers').addClass('active'); })
-					.append(TweenMax.from(sections[CAREERS].find('.sec_content'), 1, { alpha: 0, ease: Expo.easeOut }, .5))
-		, offset: -100
-		});
-
-		//contact page
-		controller.pin(sections[CONTACT], PIN_DURATION, {
-			anim: (new TimelineLite())
-					.call(function () { $('.navitem').removeClass('active'); })
-					.call(function () { $('#nav_contact').addClass('active'); })
-					.append((new TimelineLite()).append([
-						TweenMax.staggerFrom(sections[CONTACT].find('.sec_col'), 1, { y: '+600', ease: Expo.easeOut }, .4),
-						TweenLite.to($('.sec_footer'), 1, {css: {top: $(window).height()-$('.sec_footer').height()-100}}, 100)
-					]))
-					.call(function() {
-						//controller.removePin(sections[CONTACT], false);
-						//sections[CONTACT].prev().css('height', 'auto');
-						sections[CONTACT].css({minHeight:'inherit', height: $(window).height() - 200});
-						//$('#sec_extra').css('height', '50px');
-					})
-					//.call(function() { controller.removePin(sections[CONTACT]);})
-		, offset: -100
-		});
-
-		//controller.addTween($(window).height(), TweenMax.to($('#sec_contact'), .001, { css: { position: 'fixed'} }), 1);
-		TweenLite.to(window, 2, { scrollTo: { y: $('#work').position().top} })
-	}
 
 	$('.work_example').on('mouseenter', function() {
 		$(this).find('.overlay').fadeIn();
@@ -242,13 +141,11 @@ function navHighlight(tab) {
 function showLightbox(image, text, size) {
 		$('#overlay').show().on('click', function() { hideLightbox(); });
 		$('body').css({'overflow': 'hidden'});
-		///$('nav').css('margin-left', '-8px'); //because,wtf
 		var contents = '<img src="images/lightbox/' + image + '.png"/>';
 		$('<div id="lightbox"><div class="close_lbx"></div></div>').css({width: size, height: size}).append(contents).appendTo('body').center();
 		if(text != '') {
 			$('#lightbox').append($('<p></p>').html(text));
 		}
-		//TweenLite.from($('#lightbox'), .6, {css: {scaleY: 0}});
 		$('#lightbox').delegate('.close_lbx', 'click', function() {
 			hideLightbox();
 		});
@@ -270,87 +167,3 @@ jQuery.fn.center = function () {
                                                 $(window).scrollLeft()) + "px");
     return this;
 }
-
-$(function(){
-
-    //ini plugin
-
-    jQuery.event.freezeEvents = function(elem) {
-
-    	if (typeof(jQuery._funcFreeze)=="undefined")
-    		jQuery._funcFreeze = [];
-
-    	if (typeof(jQuery._funcNull)=="undefined")
-    		jQuery._funcNull = function(){ };
-
-    	// don't do events on text and comment nodes
-    	if ( elem.nodeType == 3 || elem.nodeType == 8 )
-    		return;
-
-    	var events = jQuery.data(elem, "events"), ret, index;
-
-    	if ( events ) {
-
-    		for ( var type in events )
-    		{
-    			if ( events[type] ) {
-
-    				var namespaces = type.split(".");
-    				type = namespaces.shift();
-    				var namespace = RegExp("(^|\\.)" + namespaces.slice().sort().join(".*\\.") + "(\\.|$)");
-
-    				for ( var handle in events[type] )
-    					if ( namespace.test(events[type][handle].type) ){
-    						if (events[type][handle] != jQuery._funcNull){
-    							jQuery._funcFreeze["events_freeze_" + handle] = events[type][handle];
-    							events[type][handle] = jQuery._funcNull;
-    						}
-    					}
-    			}
-
-    		}
-    	}
-    }
-
-    jQuery.event.unFreezeEvents = function(elem) {
-
-    	// don't do events on text and comment nodes
-    	if ( elem.nodeType == 3 || elem.nodeType == 8 )
-    		return;
-
-    	var events = jQuery.data(elem, "events"), ret, index;
-
-    	if ( events ) {
-
-    		for ( var type in events )
-    		{
-    			if ( events[type] ) {
-
-    				var namespaces = type.split(".");
-    				type = namespaces.shift();
-
-    				for ( var handle in events[type] )
-    					if (events[type][handle]==jQuery._funcNull)
-    						events[type][handle] = jQuery._funcFreeze["events_freeze_" + handle];
-
-    			}
-    		}
-    	}
-    }
-
-    jQuery.fn.freezeEvents = function() {
-
-    	return this.each(function(){
-    		jQuery.event.freezeEvents(this);
-    	});
-
-    };
-
-    jQuery.fn.unFreezeEvents = function() {
-
-    	return this.each(function(){
-    		jQuery.event.unFreezeEvents(this);
-    	});
-
-    };
-});
